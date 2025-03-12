15b475ce5fc15eec171401d19f8f89f0c78c8a12
Richard <lcdo.richardflores@gmail.com>
2025-03-12 15:20:29 -0300

    Footer

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,3 +1,9 @@
+import { IoLogoInstagram } from "react-icons/io5";
+import { RiTwitterXLine } from "react-icons/ri";
+import { TbBrandMeta } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { FiPhoneCall } from "react-icons/fi";
+
 const Footer = () => {
 	return (
 		<footer className="border-t py-12">
@@ -26,6 +32,96 @@ const Footer = () => {
 						</button>
 					</form>
 				</div>
+				{/* Second Column */}
+				<div>
+					<h3 className="text-lg text-gray-800 mb-4">Shop</h3>
+					<ul className="space-y-2 text-gray-600">
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								Men's Top Wear
+							</Link>
+						</li>
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								Women's Top Wear
+							</Link>
+						</li>
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								Men's Bottom Wear
+							</Link>
+						</li>
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								Women's Bottom Wear
+							</Link>
+						</li>
+					</ul>
+				</div>
+				{/* Third Column */}
+				<div>
+					<h3 className="text-lg text-gray-800 mb-4">Support</h3>
+					<ul className="space-y-2 text-gray-600">
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								Contact Us
+							</Link>
+						</li>
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								About Us
+							</Link>
+						</li>
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								FAQs
+							</Link>
+						</li>
+						<li>
+							<Link to="#" className="hover:text-gray-600 transition-colors">
+								Features
+							</Link>
+						</li>
+					</ul>
+				</div>
+				{/* Fourth Column */}
+				<div>
+					<h3 className="text-lg text-gray-800 mb-4">Follow Us</h3>
+					<div className="flex items-center  space-x-4 mb-6">
+						<a
+							href="https://www.facebook.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="hover:text-gray-500">
+							<TbBrandMeta className="h-6 w-6" />
+						</a>
+						<a
+							href="https://www.instagram.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="hover:text-gray-500">
+							<IoLogoInstagram className="h-6 w-6" />
+						</a>
+						<a
+							href="https://www.twitter.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="hover:text-gray-500">
+							<RiTwitterXLine className="h-6 w-6" />
+						</a>
+					</div>
+					<p className="text-gray-500"> Call Us</p>
+					<p>
+						<FiPhoneCall className=" inline-block mr-2 text-gray-500" />
+						0123-456-789
+					</p>
+				</div>
+			</div>
+			{/* Footer Bottom */}
+			<div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6">
+				<p className="text-gray-500 text-sm tracking-tighter text-center">
+					© 2025, RFloresDev.{" "}
+				</p>
 			</div>
 		</footer>
 	);
