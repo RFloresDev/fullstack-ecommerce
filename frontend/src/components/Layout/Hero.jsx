600e29d9c6f7479e5501e6e483f87daaf844d3f8
Richard Flores <lcdo.richardflores@gmail.com>
2025-03-12 22:27:29 -0300

    completing hero

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import heroImg from "../../assets/hero2.jpg";
+
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<section className="relative">
+			<img
+				src={heroImg}
+				alt="RF-Store"
+				className="w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover"
+			/>
+			<div className="absolute inset-0 bg-black-opacity-5 flex items-center justify-center">
+				<div className="text-center text-white p-6">
+					<h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
+						New <br /> Collection
+					</h1>
+					<p className="text-sm tracking-tight md:text-xl mb-6">
+						Fashion and Style outfits for you
+					</p>
+					<Link
+						to="#"
+						className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg
+					">
+						Shop Now
+					</Link>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,12 +1,15 @@
 import Header from "../Common/Header";
 import Footer from "../Common/Footer";
+import { Outlet } from "react-router-dom";
 
 const UserLayout = () => {
 	return (
 		<>
 			{/*Header*/}
 			<Header />
-			{/*Main content*/}
+			<main>
+				<Outlet />
+			</main>
 			<Footer />
 		</>
 	);
