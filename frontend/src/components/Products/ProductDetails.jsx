e50a76b1891a02c59a83af1c46e121b9817f1ec4
Richard Flores <lcdo.richardflores@gmail.com>
2025-03-21 17:58:13 -0300

    functionality best seller

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -23,6 +23,11 @@ const selectedProduct = {
 
 const ProductDetails = () => {
 	const [mainImage, setMainImage] = useState("");
+	const [selectedSize, setSelectedSize] = useState("");
+	const [selectedColor, setSelectedColor] = useState("");
+	const [quantity, setQuantity] = useState(1);
+	const [isButtonDisbled, setIsButtonDisabled] = useState(false);
+
 	useEffect(() => {
 		if (selectedProduct?.images?.length > 0) {
 			setMainImage(selectedProduct.images[0].url);
@@ -40,7 +45,9 @@ const ProductDetails = () => {
 								key={index}
 								src={image.url}
 								alt={image.altText || `Thumbnail ${index}`}
-								className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+								className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+									mainImage === image.url ? "border-black" : "border-gray-300"
+								}`}
 								onClick={() => setMainImage(image.url)}
 							/>
 						))}
@@ -63,6 +70,7 @@ const ProductDetails = () => {
 								src={image.url}
 								alt={image.altText || `Thumbnail ${index}`}
 								className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+								onClick={() => setMainImage(image.url)}
 							/>
 						))}
 					</div>
@@ -86,7 +94,12 @@ const ProductDetails = () => {
 								{selectedProduct.colors.map((color) => (
 									<button
 										key={color}
-										className="w-8 h-8 rounded-full border"
+										onClick={() => setSelectedColor(color)}
+										className={`w-8 h-8 rounded-full border ${
+											selectedColor === color
+												? "border-4 border-black"
+												: "border-gray-300"
+										}`}
 										style={{
 											backgroundColor: color.toLocaleLowerCase(),
 											filter: "brightness(0.5)",
@@ -115,25 +128,25 @@ const ProductDetails = () => {
 								<button className="px-2 py-2 bg-gray-200 rounded text-lg">
 									+
 								</button>
-								<button className="bg-black text-white py-2 px-6 rounded w-full mb-4">
-									ADD TO CART
-								</button>
-								<div className="mt-10 text-gray-700">
-									<h3 className="text-xl font-bold mb-4"> Characteristics:</h3>
-									<table className="w-full text-left text-sm text-gray-600">
-										<tbody>
-											<tr>
-												<td className="py-1 ">Brand:</td>
-												<td className="py-1 ">{selectedProduct.brand}</td>
-											</tr>
-											<tr>
-												<td className="py-1 ">Material:</td>
-												<td className="py-1 ">{selectedProduct.material}</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
 							</div>
+						</div>
+						<button className="bg-black text-white py-2 px-6 rounded w-full mb-4">
+							ADD TO CART
+						</button>
+						<div className="mt-10 text-gray-700">
+							<h3 className="text-xl font-bold mb-4"> Characteristics:</h3>
+							<table className="w-full text-left text-sm text-gray-600">
+								<tbody>
+									<tr>
+										<td className="py-1 ">Brand:</td>
+										<td className="py-1 ">{selectedProduct.brand}</td>
+									</tr>
+									<tr>
+										<td className="py-1 ">Material:</td>
+										<td className="py-1 ">{selectedProduct.material}</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
