21cb94e23a215cf48bb09819875305dc08166ae9
Richard <lcdo.richardflores@gmail.com>
2025-04-09 12:39:55 -0400

    Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import { Toaster } from "sonner";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<UserLayout />}>
 					<Route index element={<Home />} />
+					<Route path="/login" element={<Login />} />
 				</Route>
 				<Route>{/*Admin Layout*/}</Route>
 			</Routes>
